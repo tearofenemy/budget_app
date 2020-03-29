5f064aa6499d5e034c3c6167971c4f4e85db8c27
tearofenemy <calm.snow98@gmail.com>
2020-03-29 20:16:10 +0300

    create constructor for expense and income cash, also setup data structure for this

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const budgetController = (function() {
+var budgetController = (function() {
 
+    function Expense(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
 
+    function Income(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var data = {
+        allItems: {
+            allExp: [],
+            allInc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
 })();
 
-const UIController = (function() {
+var UIController = (function() {
 
-    const DOMStrings = {
+    var DOMStrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
@@ -13,28 +34,28 @@ const UIController = (function() {
     };
 
     return {
-        getInputs() {
+        getInputs: function() {
             return {
                 type: document.querySelector(DOMStrings.inputType).value,
                 description: document.querySelector(DOMStrings.inputDescription).value,
                 value: document.querySelector(DOMStrings.inputValue).value
             };
         },
-        getDOM() {
+        getDOM: function() {
             return DOMStrings;
         }
     };
 
 })();
 
-const controller = (function(budgetCtrl, UICtrl) {
+var controller = (function(budgetCtrl, UICtrl) {
 
-    const setupEventListeners = () => {
-        let DOM = UIController.getDOM();
+    var setupEventListeners = function() {
+        var DOM = UIController.getDOM();
 
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-        document.addEventListener('keypress', (e) => {
+        document.addEventListener('keypress', function(e) {
             if (e.keyCode === 13 || e.which === 13) {
                 ctrlAddItem();
             }
@@ -42,12 +63,12 @@ const controller = (function(budgetCtrl, UICtrl) {
     };
 
 
-    const ctrlAddItem = () => {
-        let inputs = UIController.getInputs();
+    var ctrlAddItem = function() {
+        var inputs = UIController.getInputs();
     };
 
     return {
-        init() {
+        init: function() {
             setupEventListeners();
         }
     };
